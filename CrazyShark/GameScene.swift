89e620fe4d7e92ab16c9e700387e8a25ed1097bf
Davi Chaves <dchaves@usc.edu>
2017-11-08 16:54:19 -0800

    working on movement

--- CrazyShark/GameScene.swift
+++ CrazyShark/GameScene.swift
@@ -35,87 +35,28 @@ class GameScene: SKScene {
         
     }
     
-    override func sceneDidLoad() {
-
-        self.lastUpdateTime = 0
-        
-    }
-    
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         /* Called when a touch begins */
         
-        print("test")
-        
-        shark.physicsBody?.applyImpulse(CGVector(dx: 10, dy: 0))
-        
-    }
-    
-    /*func touchDown(atPoint pos : CGPoint) {
-        print("test1")
-        
-        shark.physicsBody?.applyImpulse(CGVector(dx: 0, dy: 10))
-        
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
-    }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
+        for touch in touches {
+            let location = touch.location(in: self)
+
+            /* Reset velocity */
+            shark.physicsBody?.velocity = CGVector(dx: 0, dy: 0)
+            
+            let scale:CGFloat = 0.05
+            
+            let deltax = (location.x - shark.position.x)*scale
+            let deltay = (location.y - shark.position.y)*scale
+            
+            shark.physicsBody?.applyImpulse(CGVector(dx: deltax, dy: deltay))
         }
         
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
     }
-    
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
-    }
-    
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }*/
-    
     
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
         
-        // Initialize _lastUpdateTime if it has not already been
-        if (self.lastUpdateTime == 0) {
-            self.lastUpdateTime = currentTime
-        }
-        
-        // Calculate time since last update
-        let dt = currentTime - self.lastUpdateTime
-        
-        // Update entities
-        for entity in self.entities {
-            entity.update(deltaTime: dt)
-        }
-        
-        self.lastUpdateTime = currentTime
         
         /* Process world scrolling */
         //scrollWorld()
